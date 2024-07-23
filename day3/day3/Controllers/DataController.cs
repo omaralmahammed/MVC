ec574aa64cd7e4d2b6322e652e1b84d5bc3c6160
omaralmhammed <almahammedomar0.com>
2024-07-23 14:33:55 +0300

    My Examples for passing simple data from view to controller

---
+++ day3/day3/Controllers/DataController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace day3.Controllers
+{
+    public class DataController : Controller
+    {
+
+        public ActionResult InputInfromation(string email, string password, string firstName, string lastName)
+        {
+            ViewBag.FirstName = firstName;
+            ViewBag.LasttName = lastName;
+            ViewBag.Email = email;
+            ViewBag.Password = password;
+
+            return View();
+        }
+
+
+        public ActionResult ShowStudents(string student1, string student2, string student3)
+        {
+            ViewBag.student1 = student1;
+            ViewBag.student2 = student2;
+            ViewBag.student3 = student3;
+
+            return View();
+        }
+
+    }
+}
---
+++ day3/day3/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace day3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public ActionResult StudentsName()
+        {
+            return View();
+        }
+        /// ////////////////////////////////////
+       
+
+        [HttpGet]
+        public ActionResult Information()
+        {
+            return View();
+        }
+
+        //////////////////// USING Parameter ////////////////////
+
+
+        [HttpPost]
+        public ActionResult ShowInformation(string firstName, string lastName, string city, string email, string feedback)
+        {
+            ViewBag.FirstName = firstName;
+            ViewBag.LastName = lastName;
+            ViewBag.City = city;
+            ViewBag.Email = email;
+            ViewBag.Feedback = feedback;
+
+            return View();
+        }
+
+
+
+
+        //////////////////// USING Request ////////////////////
+
+
+        //[HttpPost]
+        //public ActionResult ShowInformation()
+        //{
+        //    ViewBag.FirstName = Request["firstName"];
+        //    ViewBag.LastName = Request["lastName"];
+        //    ViewBag.City = Request["city"];
+        //    ViewBag.Email = Request["email"];
+        //    ViewBag.Feedback = Request["feedback"];
+
+        //    return View();
+        //}
+
+
+
+        //////////////////// USING FormCollection ////////////////////
+
+        //[HttpPost]
+        //public ActionResult ShowInformation(FormCollection form)
+        //{
+        //    ViewBag.FirstName = form["firstName"];
+        //    ViewBag.LastName = form["lastName"];
+        //    ViewBag.City = form["city"];
+        //    ViewBag.Email = form["email"];
+        //    ViewBag.Feedback = form["feedback"];
+
+        //    return View();
+        //}
+
+
+
+    }
+}
