f3288afbe2b25b52ddb24ac1b4425776582986c7
omaralmhammed <almahammedomar0.com>
2024-07-24 13:24:04 +0300

    23-7-2024

---
+++ Task2/Task2/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task2.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult ContactForm()
+        {
+            return View();
+        }
+
+        public ActionResult ShowForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ShowForm(List<string> path)
+        {
+            ViewBag.FirstName = Request["name"];
+            ViewBag.ID = int.Parse(Request["id"]);
+
+            switch (Request["gender"])
+            {
+                case "male":
+                    ViewBag.Gender = "Male";
+                    break;
+                case "female":
+                    ViewBag.Gender = "Female";
+                    break;
+            }
+            
+            ViewBag.selectedGroup = Request["group"];
+
+            ViewBag.Path = path;
+            
+            return View();
+        }
+
+    }
+}
