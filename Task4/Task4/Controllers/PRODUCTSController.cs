9d4243277ec7ed9424820d04e1b805dd603747b1
omaralmhammed <almahammedomar0.com>
2024-07-27 21:18:59 +0300

    27-7-2024

---
+++ Task4/Task4/Controllers/PRODUCTSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Task4.Models;
+
+namespace Task4.Controllers
+{
+    public class PRODUCTSController : Controller
+    {
+        private MarketEntities1 db = new MarketEntities1();
+
+        // GET: PRODUCTS
+        public ActionResult Index()
+        {
+            return View(db.PRODUCTS.ToList());
+        }
+
+        // GET: PRODUCTS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT pRODUCT = db.PRODUCTS.Find(id);
+            if (pRODUCT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT);
+        }
+
+        // GET: PRODUCTS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PRODUCTS/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "product_id,product_name,poduct_quanity,product_price,product_image")] PRODUCT pRODUCT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PRODUCTS.Add(pRODUCT);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pRODUCT);
+        }
+
+        // GET: PRODUCTS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT pRODUCT = db.PRODUCTS.Find(id);
+            if (pRODUCT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT);
+        }
+
+        // POST: PRODUCTS/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "product_id,product_name,poduct_quanity,product_price,product_image")] PRODUCT pRODUCT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pRODUCT).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pRODUCT);
+        }
+
+        // GET: PRODUCTS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PRODUCT pRODUCT = db.PRODUCTS.Find(id);
+            if (pRODUCT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pRODUCT);
+        }
+
+        // POST: PRODUCTS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PRODUCT pRODUCT = db.PRODUCTS.Find(id);
+            db.PRODUCTS.Remove(pRODUCT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
